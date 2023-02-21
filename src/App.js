fc30c220c6f46ce771866bf8394a4c5f490369a0
Amit <amitmahure929@outlook.com>
2023-02-21 12:17:36 +0530

    Changes Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import Nav from './components/Header/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Header/Footer';
+import SignUp from './components/Header/SignUp';
+import PrivateComponent from './components/PrivateComponent';
+import Login from './components/Login';
+import AddProduct from './components/AddProduct';
+import ProductList from './components/ProductList';
+import UpdateProduct from './components/UpdateProduct';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route element={<PrivateComponent />}>
+            <Route path='/' element={<ProductList/>} />
+            <Route path='/add' element={<AddProduct/>} />
+            <Route path='/update/:id' element={<UpdateProduct/>} />
+            
+            <Route path='/logout' element={<h1>Logout components</h1>} />
+            <Route path='/profile' element={<h1>Profile  components</h1>} />
+          </Route>
+
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+
+        </Routes>
+      </BrowserRouter>
+
+
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
+    const getProducts = async () => {
+        let result = await fetch('http://localhost:5000/products',{
+            headers:{
+                authorization:`bearer ${JSON.parse(localStorage.getItem('token'))}`
+            }
+        });
+        result = await result.json();
+        setProducts(result);
+    }
+    // console.log("products",products);
+
+    const deleteProduct = async (id) => {
+        let result = fetch(`http://localhost:5000/product/${id}`, {
+            method: "Delete"
+        });
+        result = await result.json()
+        if (result) {
+            getProducts();
+        }
+
+    }
+    const searchHandle = async (event) => {
+        let key = event.target.value;
+        if (key) {
+            let result = await fetch(`http://localhost:5000/search/${key}`);
+            result = await result.json();
+            if (result) {
+                setProducts(result)
+            }
+        } else {
+            getProducts();
+        }
+    }
+
+    return (
+        <div className='product-list'>
+            <h1> Product List</h1>
+            <input className='search-product-box' type="text" placeholder='Search Product'
+                onChange={searchHandle}
+            />
+            <ul>
+                <li><strong>Sr. No</strong></li>
+                <li><strong>Name</strong></li>
+                <li><strong>Price</strong></li>
+                <li><strong>Category</strong></li>
+                <li><strong>Operation</strong></li>
+            </ul>
+            {
+                products.length > 0 ? products.map((item, index) =>
+                    <ul key={item.id}>
+                        <li>{index + 1}</li>
+                        <li>{item.name}</li>
+                        <li>{item.price}</li>
+                        <li>{item.category}</li>
+                        <li><button onClick={() => deleteProduct(item._id)}>Delete</button>
+                            <Link to={"/update/" + item._id}>Update</Link></li>
+                    </ul>
+                ) 
+                : <h1>No Result Found</h1>
+            }
+        </div>
+    )
+}
+export default ProductList;
